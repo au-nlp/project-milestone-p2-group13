fec625ab6028c267df692a4fb2ec0f44235b6150
Hana Spacilova <ha.spa13@gmail.com>
2025-11-05 23:19:47 +0100

    simple boxplots for all metrics-data, in the main - code duplication got better, second helper function for a bit more complicated graph

--- helper_funks.py
+++ helper_funks.py
@@ -2,10 +2,25 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
-def show_me(source):
-    return alt.Chart(source, title="ainize_bleurtScores.csv").mark_boxplot().encode(
+def show_me(source, my_title):
+    return alt.Chart(source, title=my_title).mark_boxplot().encode(
         x='column',
         y='value',
         color='column'
         
     ).properties(width=800).interactive()
+    
+    
+def show_me_more(source, my_title):
+    bar = alt.Chart(source, title=my_title).mark_bar().encode(
+    x=alt.X('Topics:N').sort(),
+    y='amount:Q',
+    tooltip='amount',
+    color='amount'
+
+    ).interactive()
+
+    rule = alt.Chart(source).mark_rule(color='magenta').encode(
+    y='mean(amount):Q'
+    )
+    return (bar + rule).properties(width=600)
