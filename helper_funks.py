a04e92d1645d1bea40cf77e1be1119b35b927c90
Hana Spacilova <ha.spa13@gmail.com>
2025-11-07 09:42:33 +0100

    charts into main and line graphs for rouge metrics

--- helper_funks.py
+++ helper_funks.py
@@ -2,7 +2,7 @@ import pandas as pd
 import altair as alt
 import numpy as np
 
-def show_me(source, my_title):
+def show_me(source, my_title): # simple box plot
     return alt.Chart(source, title=my_title).mark_boxplot().encode(
         x='column',
         y='value',
@@ -11,12 +11,12 @@ def show_me(source, my_title):
     ).properties(width=800).interactive()
     
     
-def show_me_more(source, my_title, my_color, reverse_color=True):
+def show_me_more(source, my_title, my_color, reverse_color=True): # bar chart with line showing average
     bar = alt.Chart(source, title=my_title).mark_bar().encode(
     x=alt.X('Topics:N').sort(),
     y='amount:Q',
     tooltip='amount',
-    color=alt.Color('amount').scale(scheme=my_color, reverse=reverse_color) #the color change does not work
+    color=alt.Color('amount').scale(scheme=my_color, reverse=reverse_color)
     ).interactive()
 
     rule = alt.Chart(source).mark_rule(color='magenta').encode(
